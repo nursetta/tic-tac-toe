489f896b7f6f5d942e9dd829f79016512207209b
nursetta <n_ursetta@yahoo.com>
2016-06-19 22:12:44 -0600

    super frustrated

--- java/tictactoe.js
+++ java/tictactoe.js
@@ -1,39 +1,34 @@
 window.onload = start;
 var boxes = document.getElementsByTagName("td");
-
+var playerX = 'X'; 
+var playerO = 'O'; 
+var turn = 0; 
 
 function start(){
- showX(); 
+showX(); 
 
 }
 
 function showX() {
-var td = document.getElementById('sq1');
-	for (i = 0; i < boxes.length; i++){
-		boxes[i].addEventListener('click', makeX);
-	}
+	for (var i = 0; i < boxes.length; i++){
+		boxes[i].addEventListener("click", makeX(i));
+		}
+}
+function makeX(box) {
+	if (turn === 0) {
+     boxes[box].textContent = playerX;
+            turn = 1;
+        } else {
+     boxes[box].textContent = playerO;
+            turn = 0;
+        }
+
 }
 
-function makeY() {
-	 'Y';
-}
-function makeX() {
-var td = document.getElementById('sq1');
-	td.innerHTML = 'X';
-}
-
- /*	var td = document.getElementsByTagName('td');
-		for (i = 0; i < td.length; i++)
-		td[i].innerHTML = makeX;
-} else if ('click' === false) {
-	var td2 = document.getElementsByTagName('td');
-		for (i = 0; i < td2.length; i++)
-		td2[i].innerHTML = 'X';
-}
-}
-/*function addXandOListener(){
-  for (var i = boxes.length - 1; i >= 0; i--) {
-    boxes[i].addEventListener("click", addXorO);
-  }
-}*/
-
+/* I have worked on this for two days straight and I can't figure
+out why it is pulling in x's and o's when the page loads. My click
+function works seperately and so does the function, but together
+it's almost like everything is executing all at once and not on 
+each click. I can look online and find a thousand different ways
+to make this work, but I am trying to use what we are learning in 
+class to create this game. I'm at a loss and very frustrated. */
