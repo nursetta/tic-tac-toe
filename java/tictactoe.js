23512e80b551288511d29b2b516de410293c79fd
nursetta <n_ursetta@yahoo.com>
2016-06-20 23:58:49 -0600

    working tictactoe game

--- java/tictactoe.js
+++ java/tictactoe.js
@@ -1,34 +1,49 @@
+
 window.onload = start;
 var boxes = document.getElementsByTagName("td");
 var playerX = 'X'; 
 var playerO = 'O'; 
-var turn = 0; 
+var turn = 0;
+var counter = 0; 
 
-function start(){
-showX(); 
+function start() {
+    var box = document.getElementsByTagName('td');
+    for (var i = 0; i <box.length; i++) {
+    box[i].addEventListener("click", makeX);
+	}
+
+    var reset = document.querySelector('button');
+	reset.addEventListener("click", clearBoard);
 
 }
 
-function showX() {
-	for (var i = 0; i < boxes.length; i++){
-		boxes[i].addEventListener("click", makeX(i));
-		}
-}
-function makeX(box) {
-	if (turn === 0) {
-     boxes[box].textContent = playerX;
+function makeX(square) {
+	if (square.target.textContent.length === 0) {
+	if (counter % 2 === 0) {
+     square.target.textContent = playerX;
+     square.target.setAttribute('class', 'X');
             turn = 1;
+            counter ++;
         } else {
-     boxes[box].textContent = playerO;
+     square.target.textContent = playerO;
+     square.target.setAttribute('class', 'O');
             turn = 0;
+            counter ++;
         }
-
+}
 }
 
-/* I have worked on this for two days straight and I can't figure
-out why it is pulling in x's and o's when the page loads. My click
-function works seperately and so does the function, but together
-it's almost like everything is executing all at once and not on 
-each click. I can look online and find a thousand different ways
-to make this work, but I am trying to use what we are learning in 
-class to create this game. I'm at a loss and very frustrated. */
+function clearBoard() {
+    for (var i = 0; i <boxes.length; i++) {
+    boxes[i].textContent ='';
+    boxes[i].setAttribute('class', 'clearbackground');
+}
+	turn = 0;
+	counter = 0;
+}
+
+
+
+start();
+
+
