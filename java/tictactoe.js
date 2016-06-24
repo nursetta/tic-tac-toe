ac3a5013dbecef5606702e0ed256e8fde65b559c
nursetta <n_ursetta@yahoo.com>
2016-06-23 18:57:09 -0600

    attempt at refactoring to OOP

--- java/tictactoe.js
+++ java/tictactoe.js
@@ -1,49 +1,86 @@
 
-window.onload = start;
-var boxes = document.getElementsByTagName("td");
+//window.onload = Board;
+// var boxes = document.getElementsByTagName("td");
+
+
+// function start() {
+//     var box = document.getElementsByTagName('td');
+//     for (var i = 0; i <box.length; i++) {
+//     box[i].addEventListener("click", makeX);
+// 	}
+
+//     var reset = document.querySelector('button');
+// 	reset.addEventListener("click", clearBoard);
+
+// }
+
+// function makeX(square) {
+// 	if (square.target.textContent.length === 0) {
+// 	if (counter % 2 === 0) {
+//      square.target.textContent = playerX;
+//      square.target.setAttribute('class', 'X');
+//             turn = 1;
+//             counter ++;
+//         } else {
+//      square.target.textContent = playerO;
+//      square.target.setAttribute('class', 'O');
+//             turn = 0;
+//             counter ++;
+//         }
+// }
+// }
+
+var box = document.getElementsByTagName('td');
+var reset = document.querySelector('button');
 var playerX = 'X'; 
 var playerO = 'O'; 
 var turn = 0;
 var counter = 0; 
 
-function start() {
-    var box = document.getElementsByTagName('td');
-    for (var i = 0; i <box.length; i++) {
-    box[i].addEventListener("click", makeX);
-	}
-
-    var reset = document.querySelector('button');
-	reset.addEventListener("click", clearBoard);
-
+function Board() {
+    this.square = box;
+    this.listen = reset;
 }
 
-function makeX(square) {
-	if (square.target.textContent.length === 0) {
-	if (counter % 2 === 0) {
-     square.target.textContent = playerX;
-     square.target.setAttribute('class', 'X');
-            turn = 1;
-            counter ++;
-        } else {
-     square.target.textContent = playerO;
-     square.target.setAttribute('class', 'O');
+    Board.prototype = {
+            
+        XorO: function() {
+         
+         if (this.square.textContent.length === 0) {
+
+             if (counter % 2 === 0) {
+                 this.square.textContent = playerX;
+                 this.square.setAttribute('class', 'X');
+                        turn = 1;
+                        counter ++;
+              } else {
+                 this.square.textContent = playerO;
+                 this.square.setAttribute('class', 'O');
+                        turn = 0;
+                        counter ++;
+                    }        
+            }
+        },
+            
+        listener: function() {   
+            var self = this;
+            for (var i = 0; i < this.square.length; i++) {
+                this.square[i].addEventListener("click", function() { 
+                    self.XorO();
+                });
+            }
+        },
+    
+        clearBoard: function() {
+            for (var i = 0; i <this.listen.length; i++) {
+                this.listen[i].textContent ='';
+                this.listen[i].setAttribute('class', 'clearbackground');
+            }
             turn = 0;
-            counter ++;
+            counter = 0;
         }
-}
-}
+    };
 
-function clearBoard() {
-    for (var i = 0; i <boxes.length; i++) {
-    boxes[i].textContent ='';
-    boxes[i].setAttribute('class', 'clearbackground');
-}
-	turn = 0;
-	counter = 0;
-}
+            
 
-
-
-start();
-
-
+Board();
