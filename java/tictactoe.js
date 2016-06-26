d390777abf8cd58b6d62ef1043d97a5b6edea1a2
nursetta <n_ursetta@yahoo.com>
2016-06-26 10:56:58 -0600

    reorganized listener into one start function

--- java/tictactoe.js
+++ java/tictactoe.js
@@ -11,18 +11,14 @@ Turn.prototype = {
 };
 
 window.onload = start;
-var boxes = document.getElementsByTagName("td");
-
 
 function start() {
     var box = document.getElementsByTagName('td');
+    var reset = document.querySelector('button');    
     for (var i = 0; i <box.length; i++) {
-    box[i].addEventListener("click", makeX);
-	}
-
-    var reset = document.querySelector('button');
-	reset.addEventListener("click", clearBoard);
-
+    if (box[i].addEventListener("click", makeX));
+    reset.addEventListener("click", clearBoard);	
+    }
 }
 
 function makeX(square) {
@@ -30,9 +26,8 @@ function makeX(square) {
  //        myTurn.switch(square.target);
  //    }
 
-    if (square.target.textContent.length === 0) {
-     
-     if (myTurn.marker === 'X') {
+    if (square.target.textContent.length === 0) {   
+    if (myTurn.marker === 'X') {
         square.target.textContent = myTurn.marker;
         square.target.setAttribute('class', myTurn.marker);
         myTurn.marker = 'O';
@@ -46,16 +41,14 @@ function makeX(square) {
 }
 
 function clearBoard() {
-    for (var i = 0; i <boxes.length; i++) {
-    boxes[i].textContent ='';
-    boxes[i].setAttribute('class', 'clearbackground');
+    var box = document.getElementsByTagName('td');
+    for (var i = 0; i <box.length; i++) {
+    box[i].textContent ='';
+    box[i].setAttribute('class', 'clearbackground');
 }
 	myTurn.marker = 'X';
 
 }
-
-
-
 start();
 
 
